98f3fd051804eb4287b06f63d05771ffb3a035a8
Matt Wood <matt@cratebind.com>
2019-11-08 17:16:44 -0600

    Add component fetching logic, start parsing styles from Button component

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -4,43 +4,51 @@ import '../styles/ui.css';
 declare function require(path: string): any;
 
 const App = ({}) => {
-    const textbox = React.useRef<HTMLInputElement>(undefined);
+    // const textbox = React.useRef<HTMLInputElement>(undefined);
 
-    const countRef = React.useCallback((element: HTMLInputElement) => {
-        if (element) element.value = '5';
-        textbox.current = element;
-    }, []);
+    // const countRef = React.useCallback((element: HTMLInputElement) => {
+    //     if (element) element.value = '5';
+    //     textbox.current = element;
+    // }, []);
 
-    const onCreate = React.useCallback(() => {
-        const count = parseInt(textbox.current.value, 10);
-        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
-    }, []);
+    // const onCreate = React.useCallback(() => {
+    //     const count = parseInt(textbox.current.value, 10);
+    //     parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
+    // }, []);
 
-    const onCancel = React.useCallback(() => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-    }, []);
+    // const onCancel = React.useCallback(() => {
+    //     parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
+    // }, []);
 
     React.useEffect(() => {
         // This is how we read messages sent from the plugin controller
-        window.onmessage = (event) => {
-            const { type, message } = event.data.pluginMessage;
+        window.onmessage = event => {
+            const {type, message} = event.data.pluginMessage;
             if (type === 'create-rectangles') {
                 console.log(`Figma Says: ${message}`);
-            };
-        }
+            }
+        };
     }, []);
+
+    const saveColors = () => {
+        parent.postMessage({pluginMessage: {type: 'save-colors'}}, '*');
+    };
 
     return (
         <div>
             <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
+            <h2>Minerva Design System</h2>
+            <button id="create" onClick={saveColors}>
+                Save Colors
+            </button>
+            {/* <h2>Rectangle Creator</h2>
             <p>
                 Count: <input ref={countRef} />
             </p>
             <button id="create" onClick={onCreate}>
                 Create
             </button>
-            <button onClick={onCancel}>Cancel</button>
+            <button onClick={onCancel}>Cancel</button> */}
         </div>
     );
 };
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,26 +1,75 @@
 figma.showUI(__html__);
 
-figma.ui.onmessage = (msg) => {
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
+const getComponents = (root, components = []) => {
+    root.children.forEach(child => {
+        if (child.type === 'COMPONENT') {
+            components.push(child);
         }
 
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
+        if (child.children && child.children.length > 0) {
+            getComponents(child, components);
+        }
+    });
 
-        // This is how figma responds back to the ui
-        figma.ui.postMessage({
-            type: 'create-rectangles',
-            message: `Created ${msg.count} Rectangles`,
-        });
+    return components;
+};
+
+figma.ui.onmessage = msg => {
+    // if (msg.type === 'create-rectangles') {
+    //     const nodes = [];
+
+    //     for (let i = 0; i < msg.count; i++) {
+    //         const rect = figma.createRectangle();
+    //         rect.x = i * 150;
+    //         rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+    //         figma.currentPage.appendChild(rect);
+    //         nodes.push(rect);
+    //     }
+
+    //     figma.currentPage.selection = nodes;
+    //     figma.viewport.scrollAndZoomIntoView(nodes);
+
+    //     // This is how figma responds back to the ui
+    //     figma.ui.postMessage({
+    //         type: 'create-rectangles',
+    //         message: `Created ${msg.count} Rectangles`,
+    //     });
+    // }
+    const components = getComponents(figma.root);
+    console.log(components);
+    const styles = {
+        Button: null,
+    };
+
+    components.forEach(component => {
+        if (component.name === 'Button') {
+            console.log({component});
+            const [containerStyles, textStyles] = component.children;
+            console.log({textStyles});
+            const {fills, height, cornerRadius} = containerStyles;
+            const {fontSize, fontName, letterSpacing, lineHeight} = textStyles;
+            styles.Button = {
+                container: {
+                    backgroundColor: fills[0].color,
+                    borderRadius: cornerRadius,
+                    height,
+                },
+                text: {
+                    fontSize,
+                    letterSpacing,
+                    lineHeight,
+                    fontFamily: fontName.family,
+                    fontWeight: fontName.style.toLowerCase(),
+                },
+            };
+        }
+    });
+
+    if (msg.type === 'save-colors') {
+        console.log(figma);
+        console.log({styles});
+        // console.log(figma.getLocalPaintStyles())
     }
 
-    figma.closePlugin();
+    // figma.closePlugin();
 };
